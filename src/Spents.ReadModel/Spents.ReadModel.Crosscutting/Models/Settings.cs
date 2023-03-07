e9d997f34fb6c7e5fb526ffb0b45bff24f033d36
ext.felipe.mattioli <ext.felipe.mattioli@farfetch.com>
2023-03-07 18:52:38 +0000

    FEAT: Starting adding sql config

---
+++ src/Spents.ReadModel/Spents.ReadModel.Crosscutting/Models/Settings.cs
@@ -0,0 +1,14 @@
+﻿namespace Spents.ReadModel.Crosscutting.Models
+{
+    public interface ISettings
+    {
+        public KafkaSettings? KafkaSettings { get; }
+        public SqlSettings? SqlSettings { get; }
+    }
+
+    public record Settings : ISettings
+    {
+        public KafkaSettings KafkaSettings { get; set; } = null!;
+        public SqlSettings SqlSettings { get; set; } = null!;
+    }
+}
---
+++ src/Spents.ReadModel/Spents.ReadModel.Crosscutting/Models/SqlSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Spents.ReadModel.Crosscutting.Models
+{
+    public class SqlSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+    }
+}
